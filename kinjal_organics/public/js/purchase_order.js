dd3b7aef554d2f91c8b5816efb2756ab566a61d9
rutikarathod1308 <rutikasanskartechnolab@gmail.com>
2025-05-05 18:04:20 +0530

    purchase order changes add

--- kinjal_organics/public/js/purchase_order.js
+++ kinjal_organics/public/js/purchase_order.js
@@ -7,12 +7,32 @@ frappe.ui.form.on("Purchase Order", {
 })
 
 frappe.ui.form.on("Purchase Order", {
-    onload: function (frm) {
+    before_save: function (frm) {
+        frm.doc.items.forEach(item => {
+            frappe.model.set_value(item.doctype, item.name, "custom_pending_qty", item.qty);
+        });
+    },
+})
+
+frappe.ui.form.on("Purchase Order", {
+    refresh: function (frm) {
         if (frm.doc.workflow_state === "Re-Approve") {
             
-            $("button:contains('Update Items')").hide();
-            $("button:contains('Create')").hide();
+
+            // Clear the entire 'Create' dropdown menu
+            frm.page.clear_menu();
+
+            // Try removing custom buttons (if any were added)
+            frm.remove_custom_button('Update Items');
+            
+            // Hide standard buttons via jQuery if needed (risky, not recommended for long term)
+            setTimeout(() => {
+                $("button:contains('Update Items')").hide();
+                $("button:contains('Create')").hide();
+            }, 100);
         }
     }
 });
+
+
 
