ea6d3663e52344d1461778ae28943170f9d60ad1
rutikarathod1308 <rutikasanskartechnolab@gmail.com>
2025-05-05 12:56:30 +0530

    Re-approve status than button hide

--- kinjal_organics/public/js/purchase_order.js
+++ kinjal_organics/public/js/purchase_order.js
@@ -6,4 +6,13 @@ frappe.ui.form.on("Purchase Order", {
     },
 })
 
+frappe.ui.form.on("Purchase Order", {
+    onload: function (frm) {
+        if (frm.doc.workflow_state === "Re-Approve") {
+            
+            $("button:contains('Update Items')").hide();
+            $("button:contains('Create')").hide();
+        }
+    }
+});
 
--- kinjal_organics/public/js/sales_order.js
+++ kinjal_organics/public/js/sales_order.js
@@ -5,3 +5,14 @@ frappe.ui.form.on("Sales Order", {
         });
     }
 });
+
+
+frappe.ui.form.on("Sales Order", {
+    onload: function (frm) {
+        if (frm.doc.workflow_state === "Re-Approve") {
+            
+            $("button:contains('Update Items')").hide();
+            $("button:contains('Create')").hide();
+        }
+    }
+});
